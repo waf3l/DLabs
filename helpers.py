fdfe8c1bb03e16243ef090b5ba627bbdd2856fdf
Marcin Wojtysiak <wojtysiak.marcin@gmail.com>
2021-04-06 17:46:54 +0200

    init commit

---
+++ helpers.py
@@ -0,0 +1,72 @@
+from os import path, mkdir, listdir, remove
+from datetime import datetime, timedelta
+from PIL import Image
+import time
+
+
+def prep_size(size):
+    """
+    Transforms size to propare values and type
+    
+    :param size: string with the width and heigth information like 640x480
+    :returns: size value as tuple, and also size as string
+    """
+
+    size_str = size
+    size_split = size_str.split('x')
+    size = []
+    for item in size_split:
+        size.append(int(item))
+    size = tuple(size)
+
+    return size, size_str
+
+def check_thumbnail_exist(file_path):
+    """
+    Check if thumbnail at given size of selected image exist.
+    Compare if the create hour of the file is in 1 hour from now.
+    If not we delete the file, else we can use this file
+
+    :param file_path(str): full path with included file name 
+    :returns boolean: True if we cna use it, False if we will create new file
+    """
+    if path.exists(file_path):
+
+        # time create of the file
+        time_create = datetime.utcfromtimestamp(path.getctime(file_path))
+        
+        # calculate the difference
+        difference = datetime.now() - time_create
+        
+        # check if we are between 1 hour
+        if difference > timedelta(hours=1):
+            remove(file_path)
+            return False
+        else:
+            return True
+    else:
+        return False
+
+def generate_thumbnail(src_file_path, dst_file_path, size):
+    """
+    Generate file with thumbnail
+
+    :param src_file_path: path with file name of source image 
+    :param dst_file_path: path with file name of file to create / re use 
+    :param size: tuple with information about width and heigth
+    :returns: None if everything is good, ValueError if graphic type is wrong, OSError 
+        when problrem with creating / saving file 
+    """
+    if not check_thumbnail_exist(dst_file_path):
+        try:
+            # create thumbnail
+            im = Image.open(src_file_path)
+            im.thumbnail(size)
+            im.save(dst_file_path)
+            return None
+        except ValueError as e:
+            return {"status": "error", "error_type": "ValueError", "error_msg": str(e)}
+        except OSError as e:
+            return {"status": "error", "error_type": "OSError", "error_msg": str(e)}
+    else:
+        return None
---
+++ main.py
@@ -0,0 +1,77 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+from os import path, mkdir, listdir
+from PIL import Image
+from helpers import prep_size, generate_thumbnail
+import re
+import random
+import uvicorn
+
+
+IMAGES_PATH = "./images"
+THUMBNAILS_PATH = "./thumbnails"
+THUMBNAILS_REGEX = "([0-9]{1,4})+x([0-9]{1,4})+"
+
+app = FastAPI()
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
+
+@app.post("/images")
+async def images(image: UploadFile = File(...)):
+    if not path.exists(IMAGES_PATH):
+        mkdir(IMAGES_PATH)
+
+    with open(path.join(IMAGES_PATH,image.filename),'wb+') as f:
+        f.write(image.file.read())
+        f.close()
+    return {"filename": path.join(IMAGES_PATH,image.filename)}
+
+@app.get("/images/{size}")
+async def get_thumbnail(size):
+    """ 
+    Depend on size return file with thumbnail.
+
+    If file already exist, then respons will contain cached file (cache 1h)
+
+    :param size: string with the width and heigth information like 640x480
+    :returns: thumbnail image
+
+    """
+    # validate the size given by user
+    size_validation = re.match(THUMBNAILS_REGEX, size)
+    
+    if size_validation:
+        # get proper value of size
+        size, size_str = prep_size(size_validation.string)
+
+        # get list of all uploaded images
+        files_list = listdir(IMAGES_PATH)
+
+        # select random file
+        selected_file_no = random.randint(1,len(files_list))-1
+
+        # split extension from file name
+        thumb_filename, thumb_file_extension = path.splitext(files_list[selected_file_no])
+        
+        # create thumbnail file name with size information
+        thumb_size_filename = thumb_filename+'_'+size_str+thumb_file_extension
+        
+        # prepare paths for image processing
+        src_file = path.join(IMAGES_PATH,files_list[selected_file_no])
+        dst_file = path.join(THUMBNAILS_PATH,thumb_size_filename)
+        # process the image
+        status= generate_thumbnail(src_file, dst_file, size)
+
+        if status is not None:
+            # there were an error
+            return status
+        else:
+            return FileResponse(dst_file)
+    else:
+        return {"status": "error", "message": "wrong paramater"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
